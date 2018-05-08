5708dae4db41bccdc1bb7849afe694d99122030c
Andreas Olsson <olsson.andreas@gmail.com>
2018-05-08 18:07:35 +0000

    Front page

--- front-page.php
+++ front-page.php
@@ -21,7 +21,7 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
-		<div class="row">
+		<div class="row news-frontpage">
 				<?php
 				$args = array( 'numberposts' => 6, 'offset' => 3 );
 				$recent_posts = wp_get_recent_posts($args);
@@ -42,7 +42,7 @@ $container   = get_theme_mod( 'understrap_container_type' );
 				    $count++;
 						if($count%3 == 0) {
 							echo '</div>';
-							echo '<div class="row">';
+							echo '<div class="row news-frontpage">';
 						}
 				  }
 
